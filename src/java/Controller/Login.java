89611b792391a7b5ff5c5cee37275b135e903d7e
HoangNHHE171693 <HoangNHHE171693@fpt.edu.vn>
2024-05-29 20:59:40 +0700

    update

--- src/java/Controller/Login.java
+++ src/java/Controller/Login.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package Controllers;
+package Controller;
 
+import Controllers.*;
 import DAO.UserDAO;
 import Models.User;
 import java.io.IOException;
